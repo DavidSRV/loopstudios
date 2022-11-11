bdbd3acb0c8c8a01ac7e07c29c2caaeb776c965e
David Ruiz <david.ruizvargas@hotmail.com>
2022-11-10 23:32:09 -0500

    Se hacen rutas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import NavBar from "./components/navBar/NavBar";
+import { Route, Routes } from "react-router-dom";
+import Main from "./pages/main/Main";
+import About from "./pages/about/About";
+import Careers from "./pages/careers/Careers";
+import Events from "./pages/events/Events";
+import Products from "./pages/products/Products";
+import Support from "./pages/support/Support";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <NavBar />
+
+      <Routes>
+        <Route path="/loopstudios" element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/support" element={<Support />} />
+      </Routes>
     </div>
   );
 }
